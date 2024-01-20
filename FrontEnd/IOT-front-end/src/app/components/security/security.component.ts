1992ed825ceb61fc930a1c1c930bb8480b11174e
MilosS019 <milosstanojlovic019@gmail.com>
2024-01-20 17:25:09 +0100

    Pushing to merge

--- FrontEnd/IOT-front-end/src/app/components/security/security.component.ts
+++ FrontEnd/IOT-front-end/src/app/components/security/security.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IMqttMessage, MqttService } from 'ngx-mqtt';
+import { timeout } from 'rxjs';
 
 
 @Component({
@@ -8,12 +9,12 @@ import { IMqttMessage, MqttService } from 'ngx-mqtt';
   styleUrls: ['./security.component.css']
 })
 export class SecurityComponent implements OnInit{
-  num1 : String = ""
-  num2 : String = ""
-  num3 : String = ""
-  num4 : String = ""
-  currentIndex = 0
-  correctCode : String = "1234"
+  num1 : string = ""
+  num2 : string = ""
+  num3 : string = ""
+  num4 : string = ""
+  currentIndex = -1
+  correctCode : string = "1234"
   alarmIsActive : boolean = false;
   
   constructor(private mqttService : MqttService){
@@ -22,42 +23,61 @@ export class SecurityComponent implements OnInit{
 
   ngOnInit(): void {
     this.mqttService.observe('Key').subscribe((message : IMqttMessage) => {
-      let payload = JSON.parse(message.payload.toString())
+      this.parseDMS(message)
+    });
+    this.mqttService.observe('Door Status').subscribe((message : IMqttMessage) => {
+      this.parseDS(message)
+    });
+  }
+
+  parseDMS(message : IMqttMessage){
+    console.log(message.payload.toString())
+    let payload = JSON.parse(message.payload.toString())
       if(this.currentIndex == 0){
         this.num1 = payload.value
+        this.currentIndex += 1
         this.onInput1()
       }
       else if(this.currentIndex == 1){
         this.num2 = payload.value
+        this.currentIndex += 1
         this.onInput2()
       }
       else if(this.currentIndex == 2){
         this.num3 = payload.value
+        this.currentIndex += 1
         this.onInput3()
       }
       else if(this.currentIndex == 3){
         this.num4 = payload.value
         this.currentIndex = -1
       }
-    });
+  }
+
+  parseDS(message : IMqttMessage){
+    let payload = JSON.parse(message.payload.toString())
   }
 
   onInput1(){
     document.getElementById('input2')?.focus();
-    this.currentIndex += 1
   }
   
   onInput2(){
     document.getElementById('input3')?.focus();
-    this.currentIndex += 1
   }
   
   onInput3(){
     document.getElementById('input4')?.focus();
-    this.currentIndex += 1
   }
 
   generateCode(){
     this.currentIndex = 0
+  }
+
+  activate(){
+    let fullCode = this.num1.concat(this.num2, this.num3, this.num4)
+    setTimeout(() => {
+      this.alarmIsActive = true
+    })
   }
 }
