eb4d6fbe65200a346ee959874bf54ec532c4a8bd
Anastasija Draskovic <99082640+AngieDraskovic@users.noreply.github.com>
2024-01-23 13:17:41 +0100

    Implemented ds1 and ds2 activating and deactivating alarm on front

--- FrontEnd/IOT-front-end/src/app/components/home-page/home-page.component.ts
+++ FrontEnd/IOT-front-end/src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { WebSocketService } from 'src/app/services/web-socket.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AlarmComponent } from '../alarm/alarm.component';
+import { AlarmDataService } from 'src/app/services/alarm-data.service';
 
 @Component({
   selector: 'app-home-page',
@@ -8,14 +11,19 @@ import { WebSocketService } from 'src/app/services/web-socket.service';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit{
-  constructor(private webSocketService : WebSocketService, private socket : Socket) {
-  }
+  constructor(private webSocketService: WebSocketService, 
+    private socket: Socket,
+    public dialog: MatDialog,
+    private alarmDataService: AlarmDataService) { }
   coveredPorchData: any;
   lightData:any;        // ide i on za coveredPorch ali je drugaciji child pa sam ovako
   garageData:any;
 
   doorSensorCPdata:any; // takodje za covered porch
   doorSensorGdata:any; // za garazu 
+
+  isAlarmActive = false;
+  alarmDialogRef: any; 
 
   ngOnInit(): void {
     this.webSocketService.getMessage().subscribe(data => {
@@ -28,12 +36,45 @@ export class HomePageComponent implements OnInit{
         this.garageData = data;
       }else if(data.room==='COVERED PORCH-DS'){
         this.doorSensorCPdata = data;
-      }else if(data.room==='GARAGE-DS')
+      }else if(data.room==='GARAGE-DS'){
         this.doorSensorGdata = data;
+      }else if(data.room==='ALARM'){
+        if(data.state==true){
+          this.isAlarmActive = true;
+          console.log("cao angie, UPALIOOOOOOO SE")
+          this.openOrUpdateAlarmDialog(data);
+        }else{
+          console.log("cao ANGIEE UGASIOOO SE")
+          this.isAlarmActive = false;
+          this.closeAlarmDialog();
+        }
+
       
-    })
+      }
+    }
+    )
 
     this.webSocketService.getData()
   }
 
+  openOrUpdateAlarmDialog(data: any) {
+    if (!this.alarmDialogRef || !this.alarmDialogRef.getState()) {
+      this.alarmDialogRef = this.dialog.open(AlarmComponent, {
+        width: '350px',
+        data: data
+      });
+      this.alarmDialogRef.afterClosed().subscribe(() => {
+        this.alarmDialogRef = null;
+      });
+    } else {
+      this.alarmDataService.updateAlarmData(data);
+    }
+  }
+
+  closeAlarmDialog() {
+    if (this.alarmDialogRef) {
+      this.alarmDialogRef.close();
+      this.alarmDialogRef = null;
+    }
+  }
 }
--- PI-IOT/PI/alarm.py
+++ PI-IOT/PI/alarm.py
@@ -20,18 +20,20 @@ class Alarm:
         alarm_message = {"Sensor": activation_sensor}
         print("activated: ", alarm_message)
         publish.single("ALARM ACTIVATION", json.dumps(alarm_message), hostname=HOSTNAME, port=PORT)
+        self.send_message_to_front(activation_sensor)
         # i ovdje vjr treba aktivirati db i bb - Milosev dio, mozes to preko mqtt slati db i bb-u
         # milose mislim da ce tebi trebati ovaj delay od 10 sekundi za DMS,pa vrsi provjeru ako je activation sensor dms
 
     def deactivate_alarm(self, deactivation_sensor, pin_input=""):
-        if deactivation_sensor == "DMS" and pin_input == self.pin_code: # ako ga DMS deaktivira ispraznim set aktivaionih senzora i gasim alarm definitvno
+        if deactivation_sensor == "DMS" and pin_input == self.pin_code:  # ako ga DMS deaktivira ispraznim set aktivaionih senzora i gasim alarm definitvno
             self.is_active = False
             self.is_triggered = False
             self.activated_sensors.clear()
             print("Alarm deaktiviran putem DMS-a sa ispravnim PIN-om")
             alarm_message = {"Sensor": deactivation_sensor}
             publish.single("ALARM DEACTIVATION", json.dumps(alarm_message), hostname=HOSTNAME, port=PORT)
-        elif deactivation_sensor in self.activated_sensors:    # ako su ga deaktirivirali DS1 ili DS2 gledam jesu li ga oba kako bih sigurno ugasila alarm
+            self.send_message_to_front(deactivation_sensor)
+        elif deactivation_sensor in self.activated_sensors:  # ako su ga deaktirivirali DS1 ili DS2 gledam jesu li ga oba kako bih sigurno ugasila alarm
             print(f"pokusaj deaktivacije{deactivation_sensor}")
             self.activated_sensors.remove(deactivation_sensor)
             if not self.activated_sensors:
@@ -40,11 +42,21 @@ class Alarm:
                 alarm_message = {"Sensor": deactivation_sensor}
                 print("deactivated: ", alarm_message)
                 publish.single("ALARM DEACTIVATION", json.dumps(alarm_message), hostname=HOSTNAME, port=PORT)
+                self.send_message_to_front(deactivation_sensor)
+
+    def send_message_to_front(self, last_trigger):
+        # last trigger mi cuva onoga koji ga je posljednji AKTIVIRAO a u slucaju deaktivacije ko ga je deaktivirao
+
+        message_for_front = {"room": "ALARM", "state": self.is_active, "activated_by": list(self.activated_sensors),
+                             "last_activated_by": last_trigger}
+
+        publish.single("frontend/update", payload=json.dumps(message_for_front), hostname=HOSTNAME, port=PORT)
 
 
 if __name__ == "__main__":
     alarm = Alarm()
     print("ALARM INITIATED")
+
 
     def on_message(client, userdata, message):
         if message.topic == "home/alarm/activate":
@@ -58,6 +70,7 @@ if __name__ == "__main__":
             deactivation_data = json.loads(decoded_message)
             deactivation_sensor = next(iter(deactivation_data))
             alarm.deactivate_alarm(deactivation_sensor, "")
+
 
     mqtt_client = mqtt.Client()
     mqtt_client.on_message = on_message
